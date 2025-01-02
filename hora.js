c1031d95be34c8c7c433bb36b4e4b42df536d05c
maxg100707 <maxguzman914@gmail.com>
2025-01-02 11:25:04 -0300

    Update hora.js

--- hora.js
+++ hora.js
@@ -8,15 +8,15 @@ function carregar() {
   msg.innerHTML = "Agora sao " + hora + " horas e " + min + " minutos";
   if (hora >= 0 && hora < 12) {
     //bom dia
-    img.src = "/hora/img/manha.jpg";
+    img.src = "/img/manha.jpg";
     document.body.style.backgroundColor = "#E0DF72";
   } else if (hora >= 12 && hora < 18) {
     //boa tarde
-    img.src = "/hora/img/tarde.jpg";
+    img.src = "/img/tarde.jpg";
     document.body.style.backgroundColor = "#E16E2C";
   } else {
     //boa noite
-    img.src = "/hora/img/noite.jpg";
+    img.src = "/img/noite.jpg";
     document.body.style.backgroundColor = "#273EC3";
   }
 }
